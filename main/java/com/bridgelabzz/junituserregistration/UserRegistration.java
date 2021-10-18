04b74088766137c2e6100a2f2f30fc8b5fe92809
shindeanant <shindeanant1997@gmail.com>
2021-10-18 16:29:23 +0530

    Added: User need to enter a valid Last Name

--- main/java/com/bridgelabzz/junituserregistration/UserRegistration.java
+++ main/java/com/bridgelabzz/junituserregistration/UserRegistration.java
@@ -12,6 +12,9 @@ import java.util.regex.Pattern;
  */
 
 public class UserRegistration {
+	/**
+	 * Created a method to validate first name
+	 */
 	public boolean firstName(String firstName) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
@@ -19,4 +22,13 @@ public class UserRegistration {
 		return matcher.matches();
 	}
 
+	/**
+	 * Created a method to validate last name
+	 */
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
 }
