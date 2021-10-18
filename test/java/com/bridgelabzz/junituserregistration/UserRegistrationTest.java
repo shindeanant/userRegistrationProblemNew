2f69103ffbc6b21e2490e486ce1658750d1c482c
shindeanant <shindeanant1997@gmail.com>
2021-10-18 16:22:22 +0530

    Added: Ability to perform Test of user need to enter a vid First Name

---
+++ test/java/com/bridgelabzz/junituserregistration/UserRegistrationTest.java
@@ -0,0 +1,27 @@
+package com.bridgelabzz.junituserregistration;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+	UserRegistration userRegistration;
+
+	@Before
+	public void before() {
+		userRegistration = new UserRegistration();
+	}
+
+	@Test
+	public void givenFirstNameWhenProperShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Anant");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenFirstNameWhenNotProperShouldReturnFalse() {
+		boolean result = userRegistration.firstName("anant");
+		Assert.assertFalse(result);
+	}
+
+}
